e8a056029bcd722fe194768df9eac57641c7da61
snehapar9 <snehapar@microsoft.com>
2023-11-05 17:51:28 -0800

    Test validation failures

--- azurecontainerapps.ts
+++ azurecontainerapps.ts
@@ -523,7 +523,7 @@ export class azurecontainerapps {
 
         if (this.useCliToBuildAndPushImage && !this.util.isNullOrEmpty(this.appSourcePath)) {
             this.commandLineArgs.push(`--source ${this.appSourcePath}`);
-            if (this.util.isNullOrEmpty(this.location)) {
+            if (!this.util.isNullOrEmpty(this.location)) {
                 this.commandLineArgs.push(`--location ${this.location}`);
             }
         }
--- dist/index.js
+++ dist/index.js
@@ -613,7 +613,7 @@ var azurecontainerapps = /** @class */ (function () {
         }
         if (this.useCliToBuildAndPushImage && !this.util.isNullOrEmpty(this.appSourcePath)) {
             this.commandLineArgs.push("--source " + this.appSourcePath);
-            if (this.util.isNullOrEmpty(this.location)) {
+            if (!this.util.isNullOrEmpty(this.location)) {
                 this.commandLineArgs.push("--location " + this.location);
             }
         }
