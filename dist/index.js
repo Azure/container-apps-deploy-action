e32282756c89efa66b96c74dc6b676c646588c6c
snehapar9 <snehapar@microsoft.com>
2023-11-10 13:08:56 -0800

    Remove location from up

--- dist/index.js
+++ dist/index.js
@@ -4750,7 +4750,7 @@ var ContainerAppHelper = /** @class */ (function () {
                         _a.label = 1;
                     case 1:
                         _a.trys.push([1, 3, , 4]);
-                        command_2 = "az containerapp up -n " + containerAppName + " -g " + resourceGroup + " -l eastus --debug";
+                        command_2 = "az containerapp up -n " + containerAppName + " -g " + resourceGroup + " --debug";
                         optionalCmdArgs.forEach(function (val) {
                             command_2 += " " + val;
                         });
